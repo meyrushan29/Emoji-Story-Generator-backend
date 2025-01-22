e713589d8e3f84149d400cc0b62f16837c19593c
meyrushan29 <meyrushan29@gmail.com>
2025-01-22 09:19:49 +0530

    BasicStructureOfBackend

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, Express.js Server!</h1>');
+});
+
+
+
+const port = process.env.PORT || 3000; 
+
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
